68199d1587bdbbf93c04c0e4ef79cf9703857835
PedroAugusto31 <76188013+PedroAugusto31@users.noreply.github.com>
2021-08-03 15:24:41 -0300

    theme button updated

--- public/scripts/theme.js
+++ public/scripts/theme.js
@@ -1,32 +1,42 @@
-const html = document.querySelector("html")
-const checkbox = document.querySelector("input[name=theme]")
+const html = document.querySelector("html");
+const button = document.querySelector("button.that");
+const imagem = document.querySelector("button.that img");
 
-const getStyle = (element, style) => 
-  window.getComputedStyle(element).getPropertyValue(style);
-
+const getStyle = (element, style) =>
+	window.getComputedStyle(element).getPropertyValue(style);
 
 const initialColors = {
-  background: getStyle(html, "--background"),
-  black: getStyle(html, "--black"),
-  white: getStyle(html, "--white")
- 
-}
+	background: getStyle(html, "--background"),
+	black: getStyle(html, "--black"),
+	white: getStyle(html, "--white"),
+};
 const darkMode = {
-  background: "#0d114f",
-  black: "#fafafa",
-  white: " #0d114f",
-
-
-}
-
-const transformKey = key => "--" + key.replace(/([A-Z])/, "-$1").toLowerCase()
-
-const changeColor = (color) => {
-  Object.keys(color).map(key =>
-    html.style.setProperty(transformKey(key), color[key])
-    )
+	background: "#0d114f",
+	black: "#fafafa",
+	white: " #0d114f",
 };
 
-checkbox.addEventListener("change", ({target}) => {
-  target.checked ? changeColor(darkMode) : changeColor(initialColors);
-})
+const transformKey = (key) =>
+	"--" + key.replace(/([A-Z])/, "-$1").toLowerCase();
+
+const changeColor = (color) => {
+	Object.keys(color).map((key) =>
+		html.style.setProperty(transformKey(key), color[key])
+	);
+};
+
+button.addEventListener("click", () => {
+	button.className.endsWith("-lul")
+		? (() => {
+				button.classList.remove("-lul");
+				changeColor(darkMode);
+				imagem.src =
+					"https://img.icons8.com/ios-glyphs/24/ffffff/bright-moon--v1.png";
+		  })()
+		: (() => {
+				button.classList.add("-lul");
+				changeColor(initialColors);
+				imagem.src =
+					"https://img.icons8.com/material-rounded/24/ffffff/sun--v1.png";
+		  })();
+});
